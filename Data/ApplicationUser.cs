fb1e1bbce9e42f45405a9d91ab751c1e2158296e
lhn_nryn <dubmate2244@gmail.com>
2026-04-21 12:08:03 +0545

    remove payroll, monthly, to csv reports

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -6,6 +6,5 @@ namespace HRMS.Data;
 public class ApplicationUser : IdentityUser
 {
     public string? DisplayName { get; set; }
-    public decimal DefaultHourlyRate { get; set; } = 20m;
     public ICollection<JobApplication> JobApplications { get; set; } = [];
 }
